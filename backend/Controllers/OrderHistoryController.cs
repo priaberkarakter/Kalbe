fbc9ae5a0284e9473c5f1154209160f6ab7b64c2
iei15011 <katamso.suprapto@epson.co.id>
2021-02-06 17:39:46 +0700

    initia;

---
+++ backend/Controllers/OrderHistoryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using backend.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderHistoryController : ControllerBase
+    {
+        private readonly ILogger<OrderHistoryController> _logger;
+        private readonly Database _Db;
+
+        public OrderHistoryController(ILogger<OrderHistoryController> logger, Database db)
+        {
+            _logger = logger;
+            _Db = db;
+        }
+
+        // GET: api/<OrderHistoryController>
+        [HttpGet]
+        public async Task<object> Get()
+        {
+
+            return _Db.OrderHistory.ToArray();
+        }
+
+
+    }
+}
